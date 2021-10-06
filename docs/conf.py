98b56f4fa24918d10b0e8913f70b029e30ae96af
Phytolizer <kyle.coffey@tamu.edu>
2021-10-06 13:54:03 -0500

    use xelatex

--- docs/conf.py
+++ docs/conf.py
@@ -72,6 +72,8 @@ language = None
 # This patterns also effect to html_static_path and html_extra_path
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
+latex_engine = "xelatex"
+
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = "sphinx"
 
