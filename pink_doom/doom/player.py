bd200463d2abe2653dfe0861c621900b6fbbb5e0
Phytolizer <kyle.coffey@tamu.edu>
2021-10-06 12:04:47 -0500

    Add player class

---
+++ pink_doom/doom/player.py
@@ -0,0 +1,13 @@
+"""The Doom player structure."""
+
+
+class Player:
+    """Represents the player's perspective as an object in the map."""
+
+    def __init__(self):
+        """
+        Create a new player object.
+
+        This is only likely to happen during global initialization.
+        """
+        pass
