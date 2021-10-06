e143656a46a5177647c134b8f60b4393ee1cd159
Phytolizer <kyle.coffey@tamu.edu>
2021-10-06 11:41:35 -0500

    Update setup.py for 3.9 only

--- setup.py
+++ setup.py
@@ -24,9 +24,7 @@ setup(
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
     ],
     description="Pink Doom is Doom, but in Python/Pygame!",
     entry_points={
