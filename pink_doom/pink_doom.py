569c832c14d4ccb3625749811be6845e41ca4c63
Phytolizer <kyle.coffey@tamu.edu>
2021-10-07 04:37:44 -0500

    fix silly mistake

--- pink_doom/pink_doom.py
+++ pink_doom/pink_doom.py
@@ -5,6 +5,6 @@ from pink_doom.doom.main import main
 from pink_doom.misc import argv
 
 if __name__ == "__main__":
-    argv.my_argc = len(sys.argv) - 1
+    argv.my_argc = len(sys.argv)
     argv.my_argv = sys.argv
     main()
