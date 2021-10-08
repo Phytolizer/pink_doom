3d23a3c194b3010bdc98343b3e4d4dfbd7430cff
Phytolizer <kyle.coffey@tamu.edu>
2021-10-07 21:32:10 -0500

    finish rendering/defines.py

--- pink_doom/rendering/defines.py
+++ pink_doom/rendering/defines.py
@@ -4,8 +4,10 @@ from enum import Enum, auto
 from typing import Any, Optional
 
 from pink_doom.doom.data import NodeFlag
+from pink_doom.doom.defines import SCREEN_WIDTH
 from pink_doom.misc.fixed import Fixed
 from pink_doom.playsim.mobj import MapObject
+from pink_doom.playsim.mobjflag import MapObjectFlag
 
 
 class Silhouette(Enum):
@@ -222,3 +224,162 @@ class Node:
 
     children: tuple[NodeFlag, NodeFlag]
     """If SUBSECTOR, it's a subsector."""
+
+
+@dataclass
+class Post:
+    """A run of non-masked source pixels."""
+
+    topdelta: int
+    """-1 is the last post in a column."""
+    length: int
+    """``length`` data bytes follows."""
+
+
+Column = list[Post]
+
+
+# OTHER TYPES
+
+
+LightTable = int
+
+
+@dataclass
+class DrawSeg:
+    """Docs just say "?"."""
+
+    curline: Seg
+    x1: int
+    x2: int
+
+    scale1: Fixed
+    scale2: Fixed
+
+    silhouette: Silhouette
+
+    bsilheight: Fixed
+    """Do not clip sprites above this."""
+
+    tsilheight: Fixed
+    """Do not clip sprites below this."""
+
+    # Lists for sprite clipping.
+    sprtopclip: list[int]
+    sprbottomclip: list[int]
+    maskedtexturecol: list[int]
+
+
+@dataclass
+class Patch:
+    """
+    A patch holds one or more columns.
+
+    Patches are used for sprites and all masked pictures,
+    and we compose textures from the TEXTURE1/2 lists
+    of patches.
+    """
+
+    width: int
+    height: int
+    leftoffset: int
+    """pixels to the left of origin"""
+    topoffset: int
+    """pixels below the origin"""
+
+    columnofs: list[int]
+    """length == ``width``"""
+
+
+@dataclass
+class VisSprite:
+    """
+    A vissprite_t is a thing that will be drawn during a refresh.
+
+    I.e. a sprite object that is partly visible.
+    """
+
+    x1: int
+    x2: int
+
+    # for line side calculation
+    gx: Fixed
+    gy: Fixed
+
+    # global bottom/top for silhouette clipping
+    gz: Fixed
+    gzt: Fixed
+
+    startfrac: Fixed
+    """horizontal position of x1"""
+
+    scale: Fixed
+
+    xiscale: Fixed
+    """negative if flipped"""
+
+    texturemid: Fixed
+    patch: int
+
+    colormap: LightTable
+    """for color translation and shadow draw, maxbright frames as well"""
+
+    mobjflags: MapObjectFlag
+
+
+@dataclass
+class SpriteFrame:
+    """
+    Sprites are patches with a special naming convention.
+
+    This naming convention is so that they can be recognized by
+    :func:`pink_doom.rendering.init.init_sprites`.
+
+    The base name is ``NNNNFx`` or ``NNNNFxFx``, with x indicating
+    the rotation, x = 0, 1-7.
+
+    The sprite and frame specified by a Thing is range-checked at runtime.
+
+    A sprite is a Patch that is assumed to represent a 3D object
+    and may have multiple rotations pre-drawn.
+
+    Horizontal flipping is used to save space, thus ``NNNNF2F5`` defines a
+    mirrored patch.
+
+    Some sprites will only have one picture used for all views: ``NNNNF0``
+    """
+
+    rotate: bool
+    """If false, use 0 for any position."""
+
+    lump: tuple[8 * (int,)]
+    """Lump to use for view angles 0-7."""
+
+    flip: tuple[8 * (bool,)]
+    """Flip bit to use for view angles 0-7."""
+
+
+@dataclass
+class SpriteDef:
+    """A sprite definition: a number of animation frames."""
+
+    frames: list[SpriteFrame]
+
+
+@dataclass
+class Visplane:
+    """
+    A visplane.
+
+    Now what is a visplane, anyway?
+    """
+
+    height: Fixed
+    picnum: int
+    lightlevel: int
+    minx: int
+    maxx: int
+
+    # Here lies the rub for all dynamic resize / change of resolution.
+    top: tuple[SCREEN_WIDTH * (int,)]
+    bottom: tuple[SCREEN_WIDTH * (int,)]
